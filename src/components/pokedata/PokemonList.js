e44cd30246bd5bd096878ff2351b185655d78d8b
Michael Livermore-Tozer <michael.livermore.tozer@gmail.com>
2021-06-20 01:43:30 +0100

    added generation and type filtering

--- src/components/pokedata/PokemonList.js
+++ src/components/pokedata/PokemonList.js
@@ -1,6 +1,6 @@
 import React, { Component, useState, useEffect } from 'react';
 
-import { getFullList, getPokemonUrl } from "./dataFunctions";
+import { getFullList, getPokemonUrl, getFilterUrl } from "./dataFunctions";
 import PokemonCard from './PokemonCard';
 import Pagination from '../layout/Pagination';
 
@@ -10,16 +10,29 @@ export default function PokemonList() {
   const [pokemonList, setPokemonList] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [pokemonPerPage, setPokemonPerPage] = useState(20);
+  const [filter, setFilter] = useState("Normal");
   const fullListUrl = 'https://pokeapi.co/api/v2/pokemon?limit=898';
 
   useEffect(() => {
     async function fetchData() {
-      let response = await getFullList(fullListUrl);
+      if (filter && filter.indexOf("Gen") === -1) {
+        let filterUrl = getFilterUrl(filter);
+        let response = await getFullList(filterUrl);
+        await loadPokemon(response.pokemon);
 
-      await loadPokemon(response.results);
+      } else if (filter) {
+        let filterUrl = getFilterUrl(filter);
+        let response = await getFullList(filterUrl);
+        await loadPokemon(response.results);
+
+      } else {
+        let response = await getFullList(fullListUrl);
+        await loadPokemon(response.results);
+      }
+
     }
     fetchData();
-  }, []);
+  }, [filter]);
 
   const indexOfLastPokemon = currentPage * pokemonPerPage;
   const indexOfFirstPokemon = indexOfLastPokemon - pokemonPerPage;
@@ -28,8 +41,15 @@ export default function PokemonList() {
   const loadPokemon = async (data) => {
     let _pokemon = await Promise.all(
       data.map( async pokemon => {
-        let pokemonRecord = getPokemonUrl(pokemon);
-        return pokemonRecord;
+        if (filter && filter.indexOf("Gen") === -1) {
+          let pokemonRecord = getPokemonUrl(pokemon.pokemon);
+          return pokemonRecord;
+
+        } else {
+          let pokemonRecord = getPokemonUrl(pokemon);
+          return pokemonRecord;
+
+        }
     }))
     setPokemonList(_pokemon);
     console.log(_pokemon);
@@ -39,8 +59,53 @@ export default function PokemonList() {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+  const changeFilter = (newFilter) => {
+    if (filter !== newFilter) {
+      setFilter(newFilter);
+    }
+  }
+
   return (
     <>
+      <div id="filters">
+        <div className="row generations">
+          <h3>Filter by generation:</h3>
+          <button onClick={() => changeFilter(null)} value="Full List">Full List</button>
+          <button onClick={() => changeFilter("Gen1")} value="Gen 1">Gen 1</button>
+          <button onClick={() => changeFilter("Gen2")} value="Gen 2">Gen 2</button>
+          <button onClick={() => changeFilter("Gen3")} value="Gen 3">Gen 3</button>
+          <button onClick={() => changeFilter("Gen4")} value="Gen 4">Gen 4</button>
+          <button onClick={() => changeFilter("Gen5")} value="Gen 5">Gen 5</button>
+          <button onClick={() => changeFilter("Gen6")} value="Gen 6">Gen 6</button>
+          <button onClick={() => changeFilter("Gen7")} value="Gen 7">Gen 7</button>
+          <button onClick={() => changeFilter("Gen8")} value="Gen 8">Gen 8</button>
+        </div>
+        <div className="row types">
+          <h3>Filter by type:</h3>
+          <button onClick={() => changeFilter(null)} value="All">All</button>
+          <button onClick={() => changeFilter("Normal")} value="Normal">Normal</button>
+          <button onClick={() => changeFilter("Fire")} value="Fire">Fire</button>
+          <button onClick={() => changeFilter("Water")} value="Water">Water</button>
+          <button onClick={() => changeFilter("Grass")} value="Grass">Grass</button>
+          <button onClick={() => changeFilter("Electric")} value="Electric">Electric</button>
+          <button onClick={() => changeFilter("Ice")} value="Ice">Ice</button>
+          <button onClick={() => changeFilter("Fighting")} value="Fighting">Fighting</button>
+          <button onClick={() => changeFilter("Poison")} value="Poison">Poison</button>          
+          <button onClick={() => changeFilter("Ground")} value="Ground">Ground</button>
+
+          <button onClick={() => changeFilter("Flying")} value="Flying">Flying</button>
+          <button onClick={() => changeFilter("Psychic")} value="Psychic">Psychic</button>
+          <button onClick={() => changeFilter("Bug")} value="Bug">Bug</button>
+          <button onClick={() => changeFilter("Rock")} value="Rock">Rock</button>
+          <button onClick={() => changeFilter("Ghost")} value="Ghost">Ghost</button>
+          <button onClick={() => changeFilter("Dark")} value="Dark">Dark</button>
+          <button onClick={() => changeFilter("Dragon")} value="Dragon">Dragon</button>
+          <button onClick={() => changeFilter("Steel")} value="Steel">Steel</button>          
+          <button onClick={() => changeFilter("Fairy")} value="Fairy">Fairy</button>
+
+        </div>
+
+      </div>
       <div id="pagination">
         <Pagination postsPerPage={pokemonPerPage} totalPosts={pokemonList.length} paginate={paginate}/>
       </div>
--- src/components/pokedata/dataFunctions.js
+++ src/components/pokedata/dataFunctions.js
@@ -17,3 +17,38 @@ export function getPokemonUrl({ url } ) {
             })
     });
 }
+
+export function getFilterUrl(filter) {
+    const dictionary = {
+        "Gen1": "https://pokeapi.co/api/v2/pokemon?limit=151",
+        "Gen2": "https://pokeapi.co/api/v2/pokemon?offset=151&limit=100",
+        "Gen3": "https://pokeapi.co/api/v2/pokemon?offset=251&limit=135",
+        "Gen4": "https://pokeapi.co/api/v2/pokemon?offset=386&limit=107",
+        "Gen5": "https://pokeapi.co/api/v2/pokemon?offset=493&limit=156",
+        "Gen6": "https://pokeapi.co/api/v2/pokemon?offset=649&limit=71",
+        "Gen7": "https://pokeapi.co/api/v2/pokemon?offset=721&limit=88",
+        "Gen8": "https://pokeapi.co/api/v2/pokemon?offset=809&limit=89",
+
+        "Normal": "https://pokeapi.co/api/v2/type/1",
+        "Fighting": "https://pokeapi.co/api/v2/type/2",
+        "Flying": "https://pokeapi.co/api/v2/type/3",
+        "Poison": "https://pokeapi.co/api/v2/type/4",
+        "Ground": "https://pokeapi.co/api/v2/type/5",
+        "Rock": "https://pokeapi.co/api/v2/type/6",
+        "Bug": "https://pokeapi.co/api/v2/type/7",
+        "Ghost": "https://pokeapi.co/api/v2/type/8",
+        "Steel": "https://pokeapi.co/api/v2/type/9",
+        "Fire": "https://pokeapi.co/api/v2/type/10",
+        "Water": "https://pokeapi.co/api/v2/type/11",
+        "Grass": "https://pokeapi.co/api/v2/type/12",
+        "Electric": "https://pokeapi.co/api/v2/type/13",
+        "Psychic": "https://pokeapi.co/api/v2/type/14",
+        "Ice": "https://pokeapi.co/api/v2/type/15",
+        "Dragon": "https://pokeapi.co/api/v2/type/16",
+        "Dark": "https://pokeapi.co/api/v2/type/17",
+        "Fairy": "https://pokeapi.co/api/v2/type/18",
+    }
+    console.log(dictionary[filter]);
+    return dictionary[filter];
+
+}
